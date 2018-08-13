f211acc0c047db3c1c68b4132c4748d569eaa503
League <League@AdminisorsiMac2.attlocal.net>
2018-08-13 12:00:55 -0700

    day 1 pt 3

--- src/MinionTest.java
+++ src/MinionTest.java
@@ -8,19 +8,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * To make these tests pass, you will need to create a Minion class with the member variables below.
- * 
- * <code>
- * 			private String name; 
- * 			private int eyes; 
- * 			private String color; 
- * 			private String master;
- * </code>
- * 
- * Create a constructor, and getters and setters for the member variables. If they’re done right, these tests will pass.
- * 
- * **/
+
+// * To make these tests pass, you will need to create a Minion class with the member variables below.
+  	class Minion {
+  			private String name; 
+  			private int eyes; 
+ 			private String color; 
+ 			private String master;
+ 			Minion (String name, int eyes, String color, String master  ){
+ 				this.name = name;
+ 				this.eyes = eyes;
+ 				this.color = color;
+ 				this.master = master;
+ 			}
+ 			public String getName() {
+ 				return name;
+ 			}
+ 			public int getEyes() {
+ 				return eyes;
+ 			}
+ 			public String getColor() {
+ 				return color;
+ 			}
+ 			public String getMaster() {
+ 				return master;
+ 			}
+ 			public void setMaster(String x) {
+ 				this.master = x;
+ 			}
+ 			
+  	}
+
+// * Create a constructor, and getters and setters for the member variables. If they’re done right, these tests will pass.
+
 
 public class MinionTest {
 
